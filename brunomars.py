e1e5debf6158e1d70b04b9ea043bead6070dc93b
gegemohn <151636737+gegemohn@users.noreply.github.com>
2024-02-01 13:31:06 +0800

    Add files via upload

---
+++ brunomars.py
@@ -0,0 +1,26 @@
+import sys
+
+def loop():
+    while True:
+        response = input("WILL YOU PLEASE GO TO BRUNO MARRRRSSSS (Y/N): ")
+        if response.lower() == "y":
+            print("YESSSSSS WOOOOO UR THE BEST \nLET'S GO HERE'S THE LINK \nhttps://ticketmaster.sg/activity/detail/24sg_brunomars")
+            input ()
+            break  # Exit the loop when 'y' is entered
+        else:
+            print("WRONG ANSWER, try again >_<")
+
+def main():
+    input("Test function")
+
+    input("JOKE IT A PRANKK HAHHH HIHI KUYA JE")
+
+    input("Personally I think you are really great")
+
+    input("Pogi even!!")
+
+    input("I have a question...")
+
+    loop()
+
+main()
